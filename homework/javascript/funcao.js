fd1c9c04e64bdec680e4dbd266b22d0348589117
Sara Machuqueiro <sallymachuqueiro@gmail.com>
2025-06-19 14:50:57 +0100

    function study

---
+++ homework/javascript/funcao.js
@@ -0,0 +1,12 @@
+function parimpar(n){
+    if (n%2==0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+parimpar(4)
+let res = parimpar(4)
+//console.log(parimpar(4)) - também da pra fazer desta forma o return aparecer na tela
+console.log(res)
